34157cdf3188db9d6efc5146d58e8e49dcb456f8
Jeswin A.H <jeswinah@example.com>
2025-07-27 14:48:47 +0530

    New

---
+++ Movieapp/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import SearchPage from "./components/Searchpage";
+import MovieDetails from "./components/MovieDetail";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className=" ">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
